383f045e5ad2f639471fb70a79278a190bfd5f42
Евгения <zem.jane@mail.ru>
2019-03-07 19:47:34 +0500

    v1.2. AsyncTask ver.

---
+++ app/src/main/java/com/example/onemorechapter/model/Constants.java
@@ -0,0 +1,8 @@
+package com.example.onemorechapter.model;
+
+public class Constants {
+    public static final String CURRENT_DIR = "currentDir";
+
+    public static final String ROOT_DIR = "/";
+
+}
